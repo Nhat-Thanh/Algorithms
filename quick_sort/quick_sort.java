965f1fed037da0f91e3b9419e78581bc9a028009
Nhat-Thanh <luonkebenban@gmail.com>
2021-08-05 12:54:46 +0700

    quick sort algorithm

--- quick_sort/quick_sort.java
+++ quick_sort/quick_sort.java
@@ -1,9 +1,32 @@
-public static void name() {
-    void quickSort(int[] array, int first, int right) {
+public class quick_sort {
+    public static void quickSort(int[] array, int first, int last) {
+        int pivot = array[(first + last) >> 1];
+        int left = first;
+        int right = last;
+        while (left <= right) {
+            while (array[left] < pivot)
+                left++;
+            while (array[right] > pivot)
+                right--;
+            if (left <= right) {
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
 
+                left++;
+                right--;
+            }
+        }
+        if (first < right)
+            quickSort(array, first, right);
+        if (left < last)
+            quickSort(array, left, last);
     }
 
-    public static void main() {
-        
+    public static void main(String[] args) {
+        int[] array = { 2, 7, 8, 4, -9, 0, -100, 20, 45, 3 };
+        quickSort(array, 0, 9);
+        for (int index = 0; index < 10; ++index) 
+            System.out.printf("%d ", array[index]);
     }
 }
