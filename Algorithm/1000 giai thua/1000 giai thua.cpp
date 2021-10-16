45fbebf4647b0443b3d54ee96d1ff3a4a465a0f5
Nhat-Thanh <luonkebenban@gmail.com>
2021-10-17 00:12:18 +0700

    update algorithm

--- Algorithm/1000 giai thua/1000 giai thua.cpp
+++ Algorithm/1000 giai thua/1000 giai thua.cpp
@@ -30,28 +30,3 @@ int main() {
     std::cin >> n;
     factorial(n);
 }
-
-
-
-// void factorial(int n) {
-//     std::vector<int> digits;
-//     int i, j;
-//     int SoNho, temp, num_digits;
-//     SoNho = 0;
-//     num_digits = 1;
-//     digits.emplace_back(1);
-//     for (i = 2; i <= n; ++i) {
-//         for (j = 0; j < num_digits; ++j) {
-//             temp = (digits[j] * i + SoNho);
-//             SoNho = temp / 10;
-//             digits[j] = temp % 10;
-//         }
-//         while (SoNho > 0) {
-//             digits.emplace_back(SoNho % 10);
-//             ++num_digits;
-//             SoNho /= 10;
-//         }
-//     }
-//     for (i = num_digits - 1; i >= 0; --i)
-//         std::cout << digits[i];
-// }
